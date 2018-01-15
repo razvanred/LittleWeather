e41f5fc585919ad3c86f750e2c5e9e79caf8d8db
KayJay7 <alvisebruniera@gmail.com>
2018-01-15 18:12:17 +0100

    to Rosu

--- app/src/main/java/it/brunierarosu/littleweather/MainActivity.kt
+++ app/src/main/java/it/brunierarosu/littleweather/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import it.brunierarosu.littleweather.task.GETTask
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     companion object{
         private val TAG=MainActivity::class.java.name
+        private val secs:Long=15
     }
 
 
@@ -22,7 +25,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun login(view: View){
-        Log.i("bottone",view.toString())
-        Log.i(txtusr.text.toString(),txtpass.text.toString())
+        /*Log.i("bottone",view.toString())
+        Log.i(txtusr.text.toString(),txtpass.text.toString())*/
+       /* val int=Intent(this,SearchActivity::class.java)
+        int.putExtra("Sancio",getString(R.string.user))
+        int.putExtra("Panza",getString(R.string.password))
+
+        startActivity(int)*/
+
+        val get= GETTask()
+
+        get.execute("&q=${txtusr.text}")
+
+        Log.i("RESULT",get.get(secs, TimeUnit.SECONDS))
+
     }
 }
--- app/src/main/java/it/brunierarosu/littleweather/SearchActivity.kt
+++ app/src/main/java/it/brunierarosu/littleweather/SearchActivity.kt
@@ -2,11 +2,37 @@ package it.brunierarosu.littleweather
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        setSupportActionBar(toolbar)
+
+        tv1.text = intent.getStringExtra("Sancio")
+        tv2.text = intent.getStringExtra("Panza")
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId){
+            android.R.id.home-> run {
+                onBackPressed()
+                Log.i("Gatto", "ceppo")
+            }
+            else-> Log.i("Uncontrolled button","yes")
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
