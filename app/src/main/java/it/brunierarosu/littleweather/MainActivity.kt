78fdfc306c66387f592f0b9e188db44f0b206046
KayJay7 <alvisebruniera@gmail.com>
2018-01-15 17:13:16 +0100

    login

--- app/src/main/java/it/brunierarosu/littleweather/MainActivity.kt
+++ app/src/main/java/it/brunierarosu/littleweather/MainActivity.kt
@@ -2,9 +2,16 @@ package it.brunierarosu.littleweather
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private val TAG=MainActivity::class.java.name
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +19,10 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
+    }
+
+    fun login(view: View){
+        Log.i("bottone",view.toString())
+        Log.i(txtusr.text.toString(),txtpass.text.toString())
     }
 }
